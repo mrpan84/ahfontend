b1de79c1fdc76903fe17441e9b87f0b9e409067d
peter <pnamagonya@gmail.com>
2023-07-23 17:14:23 +0200

    Updated login.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <ion-content>
           <ion-list id="inbox-list">
             <ion-list-header>{{ store.USERTYPE }}</ion-list-header>
-            <ion-note>{{ store.USER }}</ion-note>
+            <ion-note>{{ store.USER.fname }}</ion-note>
 
             <ion-menu-toggle :auto-hide="false" v-for="(p, i) in appPages" :key="i">
               <ion-item @click="selectedIndex = i" router-direction="root" :router-link="p.url" lines="none" :detail="false" class="hydrated" :class="{ selected: selectedIndex === i }">
