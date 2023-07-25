fa19523ece544ecb07830a0efcd02d75085b0649
jerlieiv <pheselejeremiah@gmail.com>
2023-07-25 19:31:57 +0200

    added a drop down on reg form

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,75 +1,94 @@
 <template>
-    <ion-page>
-      <ion-content class="registration-content">
-        <div class="registration-container">
-          <ion-icon :icon="personAddOutline" class="registration-icon"></ion-icon>
-          <h1 class="registration-title">Create an Account</h1>
-          <div class="input-container">
-            <ion-input
-              placeholder="Full Name"
-              v-model="fullName"
-              class="input-field"
-            ></ion-input>
-            <ion-input
-              placeholder="Email"
-              v-model="email"
-              class="input-field"
-            ></ion-input>
-            <ion-input
-              placeholder="Password"
-              v-model="password"
-              :type="showPassword ? 'text' : 'password'"
-              class="input-field"
-            ></ion-input>
-            <div class="show-password-checkbox">
-              <input type="checkbox" id="showPasswordCheckbox" v-model="showPassword" class="checkbox-input">
-              <label for="showPasswordCheckbox" class="checkbox-label">Show Password</label>
-            </div>
-          </div>
-          <ion-button expand="block" @click="register" class="register-button">Register</ion-button>
-          <div class="login-link">
-            <router-link class="login" to="/folder/signin">Already have an account? Login</router-link>
+  <ion-page>
+    <ion-content class="registration-content">
+      <div class="registration-container">
+        <ion-icon :icon="personAddOutline" class="registration-icon"></ion-icon>
+        <h1 class="registration-title">Create an Account</h1>
+        <div class="input-container">
+          <ion-input
+            placeholder="Full Name"
+            v-model="fullName"
+            class="input-field"
+          ></ion-input>
+          <ion-input
+            placeholder="Email"
+            v-model="email"
+            class="input-field"
+          ></ion-input>
+          <ion-select
+            v-model="userType"
+            class="input-field ion-select"
+            :interfaceOptions="selectOptions"
+            placeholder="Select User Type"
+          >
+            <ion-select-option value="farmer">Farmer</ion-select-option>
+            <ion-select-option value="buyer">Buyer</ion-select-option>
+            <ion-select-option value="auctioneer">Auctioneer</ion-select-option>
+          </ion-select>
+          <ion-input
+            placeholder="Password"
+            v-model="password"
+            :type="showPassword ? 'text' : 'password'"
+            class="input-field"
+          ></ion-input>
+          <div class="show-password-checkbox">
+            <input type="checkbox" id="showPasswordCheckbox" v-model="showPassword" class="checkbox-input">
+            <label for="showPasswordCheckbox" class="checkbox-label">Show Password</label>
           </div>
         </div>
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script lang="ts">
-  import { IonButton, IonContent, IonIcon, IonInput, IonPage } from '@ionic/vue';
-  import { ref, defineComponent } from 'vue';
-  import { personAddOutline, eyeOutline, eyeOffOutline } from 'ionicons/icons';
-  import {useStore} from '@/Store/store.ts';
-  
-  export default defineComponent({
-    name: 'RegistrationForm',
-    components: {
-      IonButton,
-      IonContent,
-      IonIcon,
-      IonInput,
-      IonPage,
-    },
-    setup() {
-      const store = useStore();
-      const fullName = ref('');
-      const email = ref('');
-      const password = ref('');
-      const showPassword = ref(false);
-  
-      const toggleShowPassword = () => {
-        showPassword.value = !showPassword.value;
-      };
-  
-      const register = () => {
-        console.log('Register');
-      };
-  
-      return { register, fullName, email, password, showPassword, toggleShowPassword, personAddOutline, eyeOutline, eyeOffOutline };
-    },
-  });
-  </script>
-  <style scoped>
+        <ion-button expand="block" @click="register" class="register-button">Register</ion-button>
+        <div class="login-link">
+          <router-link class="login" to="/folder/signin">Already have an account? Login</router-link>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonButton, IonContent, IonIcon, IonInput, IonPage, IonSelect, IonSelectOption } from '@ionic/vue';
+import { ref, defineComponent } from 'vue';
+import { personAddOutline, eyeOutline, eyeOffOutline } from 'ionicons/icons';
+import { useStore } from '@/Store/store.ts';
+
+export default defineComponent({
+  name: 'RegistrationForm',
+  components: {
+    IonButton,
+    IonContent,
+    IonIcon,
+    IonInput,
+    IonPage,
+    IonSelect,
+    IonSelectOption,
+  },
+  setup() {
+    const store = useStore();
+    const fullName = ref('');
+    const email = ref('');
+    const password = ref('');
+    const userType = ref(''); // To store the selected user type
+    const showPassword = ref(false);
+
+    const toggleShowPassword = () => {
+      showPassword.value = !showPassword.value;
+    };
+
+    const register = () => {
+      console.log('Register');
+    };
+
+    // Optional interface options for the ion-select
+    const selectOptions = {
+      header: 'Select User Type',
+    };
+
+    return { register, fullName, email, password, userType, showPassword, toggleShowPassword, personAddOutline, eyeOutline, eyeOffOutline, selectOptions };
+  },
+});
+</script>
+
+<style scoped>
   .registration-content {
     --background: linear-gradient(to bottom right, #4F80E1, #00C9FF);
   }
@@ -143,5 +162,4 @@
   .login:hover {
     text-decoration: underline;
   }
-  </style>
-  
+</style>
