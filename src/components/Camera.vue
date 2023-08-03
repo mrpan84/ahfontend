5b3bc2ec6334150fcbf2fefc8150ed93dde857a7
jerlieiv <pheselejeremiah@gmail.com>
2023-08-03 09:36:27 +0200

    Added a Settings page

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,41 +1,60 @@
 <template>
-    <div>
-      <camera :resolution="{ width: 375, height: 812 }" ref="camera" @initialized="onCameraInitialized"></camera>
+  <div>
+    <camera :resolution="{ width: 375, height: 812 }" ref="camera" @initialized="onCameraInitialized"></camera>
+    <div v-if="cameraInitialized">
       <button @click="snapshot">Create snapshot</button>
-      <img :src="snapshotUrl" alt="Snapshot" v-if="snapshotUrl">
+      <button @click="rotateCamera">Rotate Camera</button>
     </div>
-  </template>
-  
-  <script lang="ts">
-  import Camera from "simple-vue-camera";
-  import { defineComponent, ref } from "vue";
-  
-  export default defineComponent({
-    setup() {
-      const camera = ref<InstanceType<typeof Camera>>();
-      const snapshotUrl = ref<string>("");
-  
-      const onCameraInitialized = () => {
-        // Start the camera after it has been initialized
-        camera.value?.start();
-      };
-  
-      const snapshot = async () => {
-        const blob = await camera.value?.snapshot();
-  
-        if (blob) {
-          // To show the screenshot with an image tag, create a url
-          snapshotUrl.value = URL.createObjectURL(blob);
-        }
-      };
-  
-      return {
-        camera,
-        snapshot,
-        snapshotUrl,
-        onCameraInitialized
-      };
-    }
-  });
-  </script>
-  
+    <div v-else>
+      <p>Camera Initializing...</p>
+    </div>
+    <img :src="snapshotUrl" alt="Snapshot" v-if="snapshotUrl">
+  </div>
+</template>
+
+<script lang="ts">
+import Camera from "simple-vue-camera";
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  components: {
+    Camera,
+  },
+  setup() {
+    const camera = ref<InstanceType<typeof Camera>>();
+    const snapshotUrl = ref<string>("");
+    const isFrontCamera = ref<boolean>(true);
+    const cameraInitialized = ref<boolean>(false);
+
+    const onCameraInitialized = () => {
+      // Start the camera after it has been initialized
+      camera.value?.start();
+      cameraInitialized.value = true;
+    };
+
+    const snapshot = async () => {
+      const blob = await camera.value?.snapshot();
+
+      if (blob) {
+        // To show the screenshot with an image tag, create a url
+        snapshotUrl.value = URL.createObjectURL(blob);
+      }
+    };
+
+    const rotateCamera = () => {
+      // Toggle between front and rear camera
+      isFrontCamera.value = !isFrontCamera.value;
+      camera.value?.switchCamera(isFrontCamera.value);
+    };
+
+    return {
+      camera,
+      snapshot,
+      snapshotUrl,
+      onCameraInitialized,
+      rotateCamera,
+      cameraInitialized,
+    };
+  },
+});
+</script>
--- src/views/RegistrationPage.vue
+++ src/views/RegistrationPage.vue
@@ -1,35 +1,182 @@
 <template>
- <registration-form></registration-form>
+  <ion-page>
+    <ion-content class="registration-content">
+      <div class="registration-container">
+        <ion-icon :icon="personAddOutline" class="registration-icon"></ion-icon>
+        <h1 class="registration-title">Create an Account</h1>
+        <div class="input-container">
+          <ion-input
+            placeholder="Full Name"
+            v-model="fullName"
+            class="input-field"
+          ></ion-input>
+          <ion-input
+            placeholder="Email"
+            v-model="email"
+            class="input-field"
+          ></ion-input>
+          <ion-select
+            v-model="userType"
+            class="input-field ion-select"
+            :interfaceOptions="selectOptions"
+            placeholder="Select User Type"
+          >
+            <ion-select-option value="farmer">Farmer</ion-select-option>
+            <ion-select-option value="buyer">Buyer</ion-select-option>
+            <ion-select-option value="auctioneer">Auctioneer</ion-select-option>
+          </ion-select>
+          <ion-input
+            placeholder="Password"
+            v-model="password"
+            :type="showPassword ? 'text' : 'password'"
+            class="input-field"
+          ></ion-input>
+          <div class="show-password-checkbox">
+            <input type="checkbox" id="showPasswordCheckbox" v-model="showPassword" class="checkbox-input">
+            <label for="showPasswordCheckbox" class="checkbox-label">Show Password</label>
+          </div>
+        </div>
+        <ion-button expand="block" @click="register" class="register-button">Register</ion-button>
+        <div class="login-link">
+          <router-link class="login" to="/folder/signin">Already have an account? Login</router-link>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import RegistrationForm from '@/components/Registration.vue';
+<script lang="ts">
+import { IonButton, IonContent, IonIcon, IonInput, IonPage, IonSelect, IonSelectOption } from '@ionic/vue';
+import { ref, defineComponent } from 'vue';
+import { personAddOutline, eyeOutline, eyeOffOutline } from 'ionicons/icons';
+import { useStore } from '@/Store/store.ts';
+
+export default defineComponent({
+  name: 'RegistrationForm',
+  components: {
+    IonButton,
+    IonContent,
+    IonIcon,
+    IonInput,
+    IonPage,
+    IonSelect,
+    IonSelectOption,
+  },
+  setup() {
+    const store = useStore();
+    const fullName = ref('');
+    const email = ref('');
+    const password = ref('');
+    const userType = ref(''); // To store the selected user type
+    const showPassword = ref(false);
+
+    const toggleShowPassword = () => {
+      showPassword.value = !showPassword.value;
+    };
+
+    const register = () => {
+      console.log('Register');
+    };
+
+    // Optional interface options for the ion-select
+    const selectOptions = {
+      header: 'Select User Type',
+    };
+
+    return { register, fullName, email, password, userType, showPassword, toggleShowPassword, personAddOutline, eyeOutline, eyeOffOutline, selectOptions };
+  },
+});
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
+  /* The background color for the whole content area */
+  .registration-content {
+    --background: linear-gradient(to bottom right, #4F80E1, #00C9FF);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
+  /* Container for the registration form */
+  .registration-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: sans-serif;
+    color: #fff; /* Default text color for the form */
+  }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
+  /* Icon style */
+  .registration-icon {
+    font-size: 80px;
+    color: #fff;
+    margin-top: 64px;
+  }
 
-#container a {
-  text-decoration: none;
-}
+  /* Title style */
+  .registration-title {
+    margin-top: 32px;
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  /* Input fields style */
+  .input-field,
+  ion-input.input-field {
+    width: 300px;
+    padding: 12px;
+    border-radius: 8px;
+    background-color: rgba(255, 255, 255, 0.8);
+    color: #000;
+    font-size: 16px;
+    margin-bottom: 16px;
+  }
+
+  /* ion-select style */
+  .input-field .select-text {
+    width: 100%;
+    border-radius: 8px;
+    background-color: rgba(255, 255, 255, 0.8);
+    color: #000;
+    font-size: 16px;
+    margin-bottom: 16px;
+  }
+
+  /* Show password checkbox style */
+  .show-password-checkbox {
+    display: flex;
+    align-items: center;
+    margin-top: 8px;
+  }
+
+  /* Register button style */
+  .register-button {
+    margin-top: 32px;
+    width: 300px;
+    --background: #fff;
+    --color: #4F80E1;
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  /* Login link style */
+  .login-link {
+    margin-top: 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .login {
+    color: #fff;
+    text-decoration: none;
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  /* Hover style for login link */
+  .login:hover {
+    text-decoration: underline;
+  }
 </style>
