fd2c0d8001790eaf67bd77c47f8088c5d902a6c3
jerlieiv <pheselejeremiah@gmail.com>
2023-08-19 14:08:26 +0200

    Added BreadCrumbs

---
+++ src/components/BreadCrumbsPurchases.vue
@@ -0,0 +1,25 @@
+<template>
+    <ion-breadcrumbs>
+      <ion-breadcrumb href="#home">
+        <ion-icon slot="start" :icon="home"></ion-icon>
+        Home
+      </ion-breadcrumb>
+      <ion-breadcrumb href="#purchases">
+        <ion-icon slot="start" :icon="receiptOutline"></ion-icon>
+        Purchases 
+      </ion-breadcrumb>
+    </ion-breadcrumbs>
+  </template>
+  
+  <script lang="ts">
+    import { IonBreadcrumb, IonBreadcrumbs, IonIcon, IonLabel } from '@ionic/vue';
+    import { defineComponent } from 'vue';
+    import { camera, film, flash, home, basket, hammerOutline} from 'ionicons/icons';
+  
+    export default defineComponent({
+      components: { IonBreadcrumb, IonBreadcrumbs, IonIcon, IonLabel },
+      setup() {
+        return { camera, film, flash, home, basket, receiptOutline};
+      },
+    });
+  </script>
