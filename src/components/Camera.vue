cc20630fdaa05644eee76d96dfab3358c6ea5f4c
jerlieiv <pheselejeremiah@gmail.com>
2023-08-30 14:30:01 +0300

    Added security question section on the reg form

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,60 +1,69 @@
 <template>
-  <div>
+  <div class="camera-container">
     <camera :resolution="{ width: 375, height: 812 }" ref="camera" @initialized="onCameraInitialized"></camera>
-    <div v-if="cameraInitialized">
-      <button @click="snapshot">Create snapshot</button>
+    <div v-if="cameraInitialized" class="controls">
+      <button @click="snapshot">Create Snapshot</button>
       <button @click="rotateCamera">Rotate Camera</button>
     </div>
-    <div v-else>
+    <div v-else class="loading">
       <p>Camera Initializing...</p>
     </div>
-    <img :src="snapshotUrl" alt="Snapshot" v-if="snapshotUrl">
+    <img :src="snapshotUrl" alt="Snapshot" v-if="snapshotUrl" class="snapshot-image">
   </div>
 </template>
 
-<script lang="ts">
+<script setup>
 import Camera from "simple-vue-camera";
-import { defineComponent, ref } from "vue";
+import { ref } from "vue";
 
-export default defineComponent({
-  components: {
-    Camera,
-  },
-  setup() {
-    const camera = ref<InstanceType<typeof Camera>>();
-    const snapshotUrl = ref<string>("");
-    const isFrontCamera = ref<boolean>(true);
-    const cameraInitialized = ref<boolean>(false);
+const camera = ref();
+const snapshotUrl = ref("");
+const isFrontCamera = ref(true);
+const cameraInitialized = ref(false);
 
-    const onCameraInitialized = () => {
-      // Start the camera after it has been initialized
-      camera.value?.start();
-      cameraInitialized.value = true;
-    };
+const onCameraInitialized = () => {
+  camera.value?.start();
+  cameraInitialized.value = true;
+};
 
-    const snapshot = async () => {
-      const blob = await camera.value?.snapshot();
+const snapshot = async () => {
+  const blob = await camera.value?.snapshot();
+  if (blob) {
+    snapshotUrl.value = URL.createObjectURL(blob);
+  }
+};
 
-      if (blob) {
-        // To show the screenshot with an image tag, create a url
-        snapshotUrl.value = URL.createObjectURL(blob);
-      }
-    };
-
-    const rotateCamera = () => {
-      // Toggle between front and rear camera
-      isFrontCamera.value = !isFrontCamera.value;
-      camera.value?.switchCamera(isFrontCamera.value);
-    };
-
-    return {
-      camera,
-      snapshot,
-      snapshotUrl,
-      onCameraInitialized,
-      rotateCamera,
-      cameraInitialized,
-    };
-  },
-});
+const rotateCamera = () => {
+  isFrontCamera.value = !isFrontCamera.value;
+  camera.value?.switchCamera(isFrontCamera.value);
+};
 </script>
+
+<style>
+.camera-container {
+  position: relative;
+}
+
+.controls {
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  gap: 10px;
+}
+
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.snapshot-image {
+  display: block;
+  margin-top: 20px;
+  max-width: 100%;
+  height: auto;
+}
+</style>
